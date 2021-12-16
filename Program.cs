4956f2cf6a78861201293ba6a3ee370e6390b693
pimrada 031 <61728666+pimrada641@users.noreply.github.com>
2021-12-16 12:19:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Final
+{
+    class Node{
+        protected Node next = null;
+    }
+    class Equipment : Node{
+        public Equipment Next{
+            get{return next as Equipment;}
+            set{next = value;}
+        }
+        public Equipment name;
+        public Equipment(Equipment valueName){
+            name = valueName;
+        }
+    }
+    class Combine{
+        private Equipment root = null;
+        public void Add(Equipment newEquipment){
+            if(root == null){
+                root = newEquipment;
+            }
+            else if(newEquipment < root){
+                newEquipment = root;
+                root = newEquipment;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = int.Parse(Console.ReadLine());
+            Combine combine = new Combine();
+            Equipment equips = Console.ReadLine();
+            Combine.Add(new Equipment(equips));
+        }
+    }
+}
